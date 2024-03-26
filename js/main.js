685a0239d78d4a109d86f6eb2c37bb9351bfaad3
FabrinoDash <fabrino.corp@gmail.com>
2024-03-26 10:31:09 +0300

    some issues

--- js/main.js
+++ js/main.js
@@ -1,4 +1,26 @@
 window.addEventListener('DOMContentLoaded', () => {
+    const ho  = document.querySelector('.ho');
+    const serv  = document.querySelector('.serv');
+    const cont  = document.querySelector('.cont');
+    // Toggle nav
+    ho.onclick = function(){
+        ho.classList.add('active');
+        serv.classList.remove('active');
+        cont.classList.remove('active');
+    }
+
+    serv.onclick = function(){
+        serv.classList.add('active');
+        ho.classList.remove('active');
+        cont.classList.remove('active');
+    }
+
+    cont.onclick = function(){
+        cont.classList.add('active');
+        ho.classList.remove('active');
+        serv.classList.remove('active');
+    }
+
     const menu = document.getElementById('menu');
     const down_navigation = document.getElementById('down_navigation');
     menu.onclick = () => {
